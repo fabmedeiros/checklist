db3ce4269e809d47b9e7f147f30fa6a4553acd12
Fábio Medeiros <fabmed@gmail.com>
2019-02-01 15:16:38 -0200

    Add files via upload

---
+++ caesar.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from cipher import Cipher
+
+""" Implementacao da Cifra de Cesar """
+
+class Caesar(Cipher):
+    '''
+    Cifra de Cesar
+    '''
+    def encrypt(self, texto_plano, key=3):
+        '''
+        Retorna o texto_plano cifrado com a cifra de Cesar,
+        utlizando a chave key, cujo padrao e 3.
+        '''
+        key = key % 26
+        alfabeto_cifrado = self.shift_alphabet(Cipher.plain_alphabet, key)
+
+        cipher_text = ''
+        texto_plano = self.format_str(texto_plano)
+        for char in texto_plano:
+            if char in Cipher.plain_alphabet:
+                idx = Cipher.plain_alphabet.find(char)
+                cipher_text += alfabeto_cifrado[idx]
+        return cipher_text
+
+    def decrypt(self, texto_cifrado, key=3):
+        '''
+        Retorna em texto plano o texto_cifrado decifrado com a
+        cifra de Cesar, utilizando a chave key, cujo padrao e 3.
+        '''
+        return self.encrypt(texto_cifrado, -key)
---
+++ cipher.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+from random import shuffle
+""" Classe com metodos basicos para cifras classicas """
+
+class Cipher(object):
+    """ Classe base para as cifras classicas """
+    plain_alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    plain_alphanum = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+
+    def format_str(self, text):
+        '''Retorna text sem espacos e em maiusculas'''
+        return text.replace(' ', '').upper()
+
+    def shift_alphabet(self, alphabet, shift):
+        '''Retorna alphabet com deslocamento de valor shift'''
+        return alphabet[shift:] + alphabet[:shift]
+
+    def create_square(self, alphabet = [], key = '', alphanum = False, replace = ['J', 'I'], sequence = False):
+        """ Retorna um alfabeto numa matriz de num x num
+        Por padrao, retorna uma matriz formada pelo alfabeto ABCDEFGHIKLMNOPQRSTUVWXYZ
+        Se key, retorna um square com key iniciando o square
+        alphanum square com letras e numeros
+        replace letras a serem trocadas, so funciona se for usado somente o alfabeto
+        sequence se True continua a preencher o square a partir do ultimo caracter da key
+        """
+        square = []
+        if alphabet:
+            if alphanum:
+                replace = ['', '']
+            # num = 5
+            alfabeto = alphabet
+        elif alphanum:
+            # num = 6
+            alfabeto = self.plain_alphanum
+            replace = ['', '']
+        else:
+            # num = 5
+            alfabeto = self.plain_alphabet
+        alfabeto = self.create_alphabet(key.upper(), alfabeto, replace, sequence)##
+        num = 5 + len(alfabeto) % 5
+        for idx in range(0, len(alfabeto), num):
+            square.append(alfabeto[idx:idx + num])
+        return square
+
+    def create_alphabet(self, key = '', alfabeto = plain_alphabet, replace = ['', ''], sequence = False):
+        """ Retorna um alfabeto com key como chave e no inicio do alfabeto """
+        if key:
+            key = key_repeated(key)
+            if replace[0] in key:
+                key = key.replace(replace[0], replace[1])
+            if sequence:
+                idx = alfabeto.index(key[-1])
+                alfabeto = self.shift_alphabet(alfabeto, idx)
+        cipher = alfabeto.replace(replace[0], '')
+        for ch_key in key:
+            if ch_key in cipher:
+                cipher = cipher.replace(ch_key, '')
+        return key + cipher
+
+    def random_alphabet(self, alphanum=False):
+        """ Retorna um alfabeto aleatório """
+        if alphanum:
+            alfabeto = list(self.create_alphabet(alfabeto=self.plain_alphanum))
+        else:
+            alfabeto = list(self.create_alphabet())
+        shuffle(alfabeto)
+        return ''.join(alfabeto)
+
+    def key_repeated(self, key):
+        ''' Remove caracteres repetidos da senha key '''
+        temp = ''
+        for ch in key.upper():
+            if ch not in temp:
+                temp += ch
+        return temp
